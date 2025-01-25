0f596b43306381de62d20dbbdccdb3c00f863d2b
installer <installer@gmail.com>
2025-01-25 07:06:01 +0700

    move into hardware

--- hosts/default.nix
+++ hosts/default.nix
@@ -5,10 +5,8 @@
         specialArgs = { inherit inputs; };
         system = "x86_64-linux"; # Sesuaikan dengan arsitektur sistem
         modules = [
-          ../hardware-configuration.nix
-          # Impor konfigurasi sistem yang ada
+          ./hardware
           ../configuration.nix
-          # Tambahkan modul tambahan di sini
         ];
       };
 
