6a232367e1f59fb3292055ee71c54d1b34e47e86
Wahyu Setiawan <wahyu.creator911@gmail.com>
2025-01-28 05:56:09 +0700

    udate into i

--- hosts/modules/utils/fvm.nix
+++ hosts/modules/utils/fvm.nix
@@ -1,14 +1,21 @@
-let
-  pkgs = import <nixpkgs> { };
-in
+{ pkgs, ... }:
 pkgs.stdenv.mkDerivation {
   pname = "fvm";
-  version = "0.5.1";
+  version = "latest";
+
+  # src = pkgs.fetchFromGitHub {
+  #   owner = "fvm-sh";
+  #   repo = "fvm";
+  #   rev = "main";
+  #   sha256 = "j70SOyuEQ61Ku82Z2GYieh/ALiKRXxe3GygDbz7OAuo="; # Ganti dengan checksum
+  # };
 
   src = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/fvm-sh/fvm/v0.5.1/install.sh";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Ganti dengan checksum
+    url = "https://fvm.app/install.sh";
+    sha256 = "sha256-Qubwqm6PiqfLyQAvLHxzEFELosQAE+uXzF1CX0WuMKk="; # Ganti dengan checksum yang benar
   };
+
+  unpackPhase = "true";
 
   nativeBuildInputs = [ pkgs.bash pkgs.curl pkgs.coreutils ];
 
@@ -17,8 +24,15 @@ pkgs.stdenv.mkDerivation {
   '';
 
   installPhase = ''
+    # Menjalankan script install.sh
     mkdir -p $out/bin
-    bash ./install.sh
+    mkdir -p $out/fvm
+
+    sed -i "s/FVM_DIR=\".*\"/FVM_DIR=\"$out/fvm\"" $src 
+    sed -i "s/SYMLINK_TARGET=\".*\"/SYMLINK_TARGET=\"$out/bin\"" $src 
+    bash $src
+
+    # Memastikan fvm tersedia di $out/bin
     cp -r ~/.fvm/bin/* $out/bin/
   '';
 
