7739c5d7a4c8e4f9ec17d98f40d9149628bbc330
installer <installer@gmail.com>
2025-01-29 03:44:52 +0700

    refactor audio network in folder services

--- myNix/modules/default.nix
+++ myNix/modules/default.nix
@@ -13,7 +13,7 @@
     sysetm-nixos-activations = import ./conf-linux/activations.nix;
     system-nixos-networking = import ./conf-linux/networking.nix;
     system-nixos-system = import ./conf-linux/system.nix;
-    system-nixos-services = import ./conf-linux/services.nix;
+    system-nixos-services = import ./conf-linux/services;
     system-nixos-packages = import ./conf-linux/packages.nix;
     system-nixos-programs = import ./conf-linux/programs;
     system-nixos-hardware = import ./conf-linux/hardware;
