9c7e39e8ce4e31a097ae5011c99bb32b13845330
Wahyu Setiawan <wahyu.creator911@gmail.com>
2025-01-29 07:53:21 +0700

    remove unused installation homebrew

--- myNix/hosts/default.nix
+++ myNix/hosts/default.nix
@@ -26,30 +26,6 @@ let
         ]
       ]
       ++ [
-        # {
-        #   nix-homebrew = {
-        #     # Install Homebrew under the default prefix
-        #     enable = true;
-        #
-        #     # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-        #     enableRosetta = true;
-        #
-        #     # User owning the Homebrew prefix
-        #     user = "yourname";
-        #
-        #     # Optional: Declarative tap management
-        #     taps = {
-        #       "homebrew/homebrew-core" = inputs.homebrew-core;
-        #       "homebrew/homebrew-cask" = inputs.homebrew-cask;
-        #       "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
-        #     };
-        #
-        #     # Optional: Enable fully-declarative tap management
-        #     #
-        #     # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-        #     mutableTaps = false;
-        #   };
-        # }
         ({ pkgs, ... }: {
           nixpkgs = removeAttrs ctx.nixpkgs [ "hostPlatform" ];
           users.users.${"wahyu"} = {
