ca52d50a06c61e03a1e20a83e90493fdbff621d0
installer <installer@gmail.com>
2025-02-09 21:36:52 +0700

    update

--- myNix/home/hyprland/rofi.nix
+++ myNix/home/hyprland/rofi.nix
@@ -5,12 +5,12 @@ let
 in
 {
   config = lib.mkIf hyprlandEnable {
-    home.file.".config/rofi".source = pkgs.fetchFromGitHub
-      {
-        owner = "catppuccin";
-        repo = "rofi";
-        rev = "main";
-        hash = "sha256-zA8Zum19pDTgn0KdQ0gD2kqCOXK4OCHBidFpGwrJOqg=";
-      } + "/deathemonic";
+    # home.file.".config/rofi".source = pkgs.fetchFromGitHub
+    #   {
+    #     owner = "catppuccin";
+    #     repo = "rofi";
+    #     rev = "main";
+    #     hash = "sha256-zA8Zum19pDTgn0KdQ0gD2kqCOXK4OCHBidFpGwrJOqg=";
+    #   } + "/deathemonic";
   };
 }
