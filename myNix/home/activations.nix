2adcfb2559bd51af814f913527df56073d5be434
installer <installer@gmail.com>
2025-02-09 21:19:34 +0700

    update hyprland and activations rofi

--- myNix/home/activations.nix
+++ myNix/home/activations.nix
@@ -1,11 +1,15 @@
-{ config, pkgs, ... }:let
+{ config, pkgs, ... }:
+let
   homePathConfig = config.home.user-info.nixConfigDirectory;
-in  {
-  home.activation.stowConfig = config.lib.dag.entryAfter [ "writeBoundary" ] ''
-    echo "Running stow to symlink configuration files..."
+in
+{
+  home.activation.stowConfig = config.lib.dag.entryAfter [ "writeBoundary" ] #bash
+    ''
+      echo "Running stow to symlink configuration files..."
 
-    echo "Setting configuration nvim with stow"
-    ln -sf ${homePathConfig}/dotfiles/.config/sketchybar $HOME/.config/sketchybar
-    ln -sf ${homePathConfig}/dotfiles/.config/nvim $HOME/.config/nvim
-  '';
+      echo "Setting configuration nvim with stow"
+      ln -sf ${homePathConfig}/dotfiles/.config/sketchybar $HOME/.config/sketchybar
+      ln -sf ${homePathConfig}/dotfiles/.config/nvim $HOME/.config/nvim
+      ln -sf ${homePathConfig}/dotfiles/.config/rofi $HOME/.config/rofi
+    '';
 }
