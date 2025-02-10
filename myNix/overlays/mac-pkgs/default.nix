d17872b847fbee460cf1e9131dbc471457923414
Wahyu Setiawan <wahyu.creator911@gmail.com>
2025-02-10 13:37:47 +0700

    add config sketchybar

---
+++ myNix/overlays/mac-pkgs/default.nix
@@ -0,0 +1,41 @@
+{ ... }:
+{
+  flake.overlays.macos =
+    final: prev:
+    let
+      inherit (prev.lib) attrsets;
+      callPackage = prev.newScope { };
+      packages = [
+        "xbar"
+        # "obs-studio"
+        "orbstack"
+        "telegram"
+        "iriun-webcam"
+        "clipy"
+        "sf-symbols"
+        # "googlechrome" # see system/darwin/homebrew.nix
+      ];
+    in
+    {
+      # sbar_menus = prev.callPackage ../../nixosModules/darwin/sketchybar/helpers/menus { };
+      # sbar_events = prev.callPackage ../../nixosModules/darwin/sketchybar/helpers/event_providers { };
+      sbarLua = prev.callPackage ../../modules/conf-darwin/sketchybar/sbar.nix { };
+      sketchybarConfigLua = prev.callPackage ../../modules/conf-darwin/sketchybar/config.nix { };
+      # sf-symbols-font = final.sf-symbols.overrideAttrs (old: {
+      #   pname = "sf-symbols-font";
+      #   installPhase = ''
+      #     runHook preInstall 
+      #
+      #     mkdir -p $out/share/fonts
+      #     cp -a Library/Fonts/* $out/share/fonts/
+      #
+      #     runHook postInstall
+      #   '';
+      #
+      #   meta = old.meta // {
+      #     description = "sf-symbols-font";
+      #   };
+      # });
+    };
+}
+
