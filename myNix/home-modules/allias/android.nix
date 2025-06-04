53a4a27c355238b1903ba5ed05a2f9eaa85b3877
installer <installer@gmail.com>
2025-06-04 17:05:05 +0700

    udpate alias android rust

--- myNix/home-modules/allias/android.nix
+++ myNix/home-modules/allias/android.nix
@@ -1,10 +1,11 @@
 { ... }: {
-  cleanAndroidGradle = ''
-    # Hapus cache Gradle
-    rm -rf ~/.gradle/caches/
+  cleanAndroidGradle = #bash 
+    ''
+      # Hapus cache Gradle
+      rm -rf ~/.gradle/caches/
 
-    # Hapus build Android yang menumpuk
-    find ~ -name "build" -type d -prune -exec rm -rf '{}' +
-  '';
+      # Hapus build Android yang menumpuk
+      find ~ -name "build" -type d -prune -exec rm -rf '{}' +
+    '';
 
 }
--- myNix/home-modules/allias/rust.nix
+++ myNix/home-modules/allias/rust.nix
@@ -6,5 +6,4 @@
     # Hapus target/debug dari proyek yang tidak aktif
     find ~ -name "target" -type d -prune -exec rm -rf '{}' +
   '';
-
 }
