6631ac9e3d89392f7915fc06aacecab28ef3a21e
installer <installer@gmail.com>
2025-06-11 12:06:28 +0700

    add secrets for digitalsiber and github

--- myNix/home-modules/secrets.nix
+++ myNix/home-modules/secrets.nix
@@ -1,15 +1,13 @@
-{config,  osConfig, ... }:
+{ config, osConfig, inputs, ... }:
 let
-    nixConfigDirectory = config.home.user-info.nixConfigDirectory;
-
   username = osConfig.users.primaryUser.username;
 in
 {
-  sops.defaultSopsFile = "${nixConfigDirectory}/secrets/secrets.yaml"; 
+  sops.defaultSopsFile = "${inputs.self}/secrets/secrets.yaml";
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
 
-  sops.secrets.ssh_gitlab = {
-    # path = "/home/${username}/.ssh/ssh-gitlab-juragankoding";
-  };
+  sops.secrets.ssh_gitlab = { };
+  sops.secrets.ssh_github = { };
+  sops.secrets.ssh_gitlab_siber = { };
 }
