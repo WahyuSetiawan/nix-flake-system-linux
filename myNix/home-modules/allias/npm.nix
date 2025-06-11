cfdd64cb4f047bf66ef039736b8b08ca1810f4eb
installer <installer@gmail.com>
2025-06-11 19:35:58 +0700

    update shell

--- myNix/home-modules/allias/npm.nix
+++ myNix/home-modules/allias/npm.nix
@@ -1,15 +1,35 @@
 { ... }: {
-  cleanNpm = ''
-    # Bersihkan cache npm/yarn
-        npm cache clean --force
-        yarn cache clean
+  cleanNpm = #bash 
+    ''
+      # Bersihkan cache npm/yarn
+      nix-shell -p nodejs_20 --run "npm cache clean --force"
+      nix-shell -p yarn --run "yarn cache clean"
+      nix-shell -p pnpm --run "pnpm cache delete"
 
-    # Hapus node_modules yang tidak perlu
-        find ~ -name "node_modules" -type d -prune -exec rm -rf '{}' +
+      # Hapus node_modules yang tidak perlu
+      find ~ -name "node_modules" -type d -prune -exec rm -rf '{}' +
 
-    # Hapus .npm/_logs (log error npm)
-        rm -rf ~/.npm/_logs
-  '';
-
-
+      # Hapus .npm/_logs (log error npm)
+      rm -rf ~/.npm/_logs
+    '';
+  mkVue = #bash
+    ''
+      nix-shell -p nodejs_20 --run "npm create vue@latest"
+    '';
+  mkNextJs = #bash
+    ''
+      nix-shell -p nodejs_20 --run "npx create-next-app@latest"
+    '';
+  mkNodeJs = #bash
+    ''
+      nix-shell -p nodejs_20 --run "npx express-generator"
+    '';
+  mkNuxtJs = #bash
+    ''
+      nix-shell -p nodejs_20 --run "npm create nuxt "
+    '';
+  mkAstro = #bash
+    ''
+      nix-shell -p nodejs_20 --run "npm create astro@latest"
+    '';
 }
--- myNix/home-modules/allias/python.nix
+++ myNix/home-modules/allias/python.nix
@@ -1,10 +1,11 @@
-{...}:{
-    cleanPythonPip = ''
-        # Hapus cache pip
-    pip cache purge
+{ ... }: {
+  cleanPythonPip = #bash 
+    ''
+      # Hapus cache pip
+      nix-shell -p python313Packages.pip --run "pip cache purge"
 
-    # Hapus __pycache__ dan .pyc
-    find ~ -name "__pycache__" -type d -prune -exec rm -rf '{}' +
-    find ~ -name "*.pyc" -delete
-  '';
+      # Hapus __pycache__ dan .pyc
+      find ~ -name "__pycache__" -type d -prune -exec rm -rf '{}' +
+      find ~ -name "*.pyc" -delete
+    '';
 }
