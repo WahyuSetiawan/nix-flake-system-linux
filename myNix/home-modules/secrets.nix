b0a6f6727d6371114c0ad32463925ea4eef21cd4
Wahyu Setiawan <wahyu.creator911@gmail.com>
2025-06-12 18:17:32 +0700

    udpate secrets with match mac and nix flake update

--- myNix/home-modules/secrets.nix
+++ myNix/home-modules/secrets.nix
@@ -1,15 +1,12 @@
 { config, osConfig, inputs, pkgs, ... }:
 let
   username = osConfig.users.primaryUser.username;
+  home = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
 in
 {
   sops.defaultSopsFile = "${inputs.self}/secrets/secrets.yaml";
   sops.defaultSopsFormat = "yaml";
-  sops.age.keyFile =
-    if pkgs.stdenv.isDarwin then
-      "/Users/${username}/.config/sops/age/keys.txt" else
-      "/home/${username}/.config/sops/age/keys.txt";
-
+  sops.age.keyFile = "${home}/.config/sops/age/keys.txt";
 
   sops.secrets.ssh_gitlab = { };
   sops.secrets.ssh_github = { };
