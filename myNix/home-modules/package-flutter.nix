f0fb30b3b627a6d9a637fb2933a6896c658ea798
installer <installer@gmail.com>
2025-07-14 09:02:33 +0700

    add package flutter

---
+++ myNix/home-modules/package-flutter.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    #editor
+    android-studio
+
+    scrcpy
+    fvm
+
+    jdk17
+
+    #linux tool chain
+    cmake
+    ninja
+    clang
+    pkg-config
+    gtk3
+
+    firebase-tools
+
+    google-chrome
+
+    xorg.libX11
+    xorg.libXext
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libXi
+
+    libxkbcommon
+    libglvnd
+    fontconfig
+    ncurses5
+  ];
+
+   home.sessionVariables = {
+    JAVA_HOME = "${pkgs.jdk17}";
+  };
+}
